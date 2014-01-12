71230e8e2d8af21b6a341199dc1ad44cd29d1e38
Julie <julie.d.rose@gmail.com>
2014-01-12 14:24:37 -0800

    adds tree map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
+//= require_tree .
+//= require bootstrap
 //= require jquery.turbolinks
 //= require_tree .
--- app/assets/javascripts/trees.js
+++ app/assets/javascripts/trees.js
@@ -1,10 +1,12 @@
-var bbox_map = L.mapbox.map('bbox', 'examples.map-9ijuk24y').setView([45.5, -122.5], 10);
+$(document).ready(function() {
+  var bbox_map = L.mapbox.map('bbox', 'examples.map-9ijuk24y').setView([45.5, -122.5], 10);
 
-$.ajax({
-  type: 'GET',
-  url: 'http://civicapps.iknuth.com/data/Heritage_Trees_pdx.geojson?limit=0',
-  dataType: 'jsonp',
-  success: function(geojson) {
-    var markerLayer = L.geoJson(geojson).addTo(bbox_map);
-  }
+  $.ajax({
+    type: 'GET',
+    url: 'http://civicapps.iknuth.com/data/Heritage_Trees_pdx.geojson?limit=0',
+    dataType: 'jsonp',
+    success: function(geojson) {
+      var markerLayer = L.geoJson(geojson).addTo(bbox_map);
+    }
+  });
 });
